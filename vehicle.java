c9a3d3b3e90141079357429db573d7ef35d84214
sibiyon_2200 <loviyonjosh2200@gmail.com>
2025-04-08 02:05:09 -0700

    Add files via upload

---
+++ vehicle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class vehicle{
+string mileage;
+int price;
+vehicle(string mil,int {
+mileage=mil;
+price=pri;
+}
+}
+class car extends vehicle{
+string warranty;
+double ownercost;
+int seatingcapacity;
+string fuel;
+car(string mil, in pri, string w,double owner, int capa,string f){
+super(mil,pri);
+warrenty=w;
+ownercost=owner;
+seatingcapacity = capa;
+fuel = f;
+}
+}
+public class maruthi extends car{
+string modeltype;
+maruthi(string mil, int pri, string w,double owner,int capa, string f, string model){
+super
